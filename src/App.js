b7245591b1496bab7d56af02512304b70afb3957
THULIRSHAN98 <thulirshan@iykons.com>
2024-04-04 15:27:30 +0530

    update something for ref

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} /> {/* Use curly braces to pass element prop */}
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
     </div>
