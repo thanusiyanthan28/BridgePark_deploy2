95e73b5084ef4018ef835b83c22febdc130ac70e
thanusiyanthan28 <thanusiyanthan@iykons.com>
2024-04-05 10:54:09 +0545

    add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Header from './components/common/Header'
+import Footer from "./components/common/Footer";
 
 export default function App() {
   return ( 
@@ -11,6 +12,7 @@ export default function App() {
           <Route path="/" element={<HomePage />} /> {/* Use curly braces to pass element prop */}
         </Routes>
       </Router>
+      {<Footer/> }
     </div>
   );
 }
