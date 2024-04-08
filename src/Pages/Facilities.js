202bc295a20b7d5c7388e9b04b5376fe1f4cadec
Dussanthan01 <dussanthan99@iykons.com>
2024-04-08 17:46:48 +0530

    facilities

--- src/Pages/Facilities.js
+++ src/Pages/Facilities.js
@@ -5,18 +5,18 @@ import ColumnList from "../components/Facilities/facilitiesList"
 import { faWifi, faParking, faGlassMartiniAlt, faUtensils, faTv, faBed, faSmokingBan, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 const Card = ({ title, icon }) => (
-  <div className="facilities-card">
-    <div className='facilities-icon-container'>
+  <div className="facilitiescard">
+    <div className='facilitiesiconcontainer'>
       <FontAwesomeIcon icon={icon} size="4x" color="#618e95" style={{ imageRendering: 'pixelated' }} />
     </div>
-    <div className="facilities-card-content">
+    <div className="facilitiescardcontent">
       <h2>{title}</h2>
     </div>
   </div>
 );
 
 const CardContainer = ({ cards, icons }) => (
-  <div className='facilities-cards-container'>
+  <div className='facilitiescardscontainer'>
     {cards.map((card, index) => (
       <Card
         key={card.id}
@@ -49,14 +49,14 @@ const Facilities = () => {
   }
 
   return (
-    <body className='facilities-body'>
-    <div className='facilities-main'>
+    <body className='facilitiesbody'>
+    <div className='facilitiesmain'>
       <div>
-        <h1 className='facilities-head'>FACILITIES</h1>
+        <h1 className='facilitieshead'>FACILITIES</h1>
       </div>
 
       <div>
-        <p className='facilities-para'>
+        <p className='facilitiespara'>
           Notice of a bid advertisement shall be published in at
           least one local newspaper and in one trade publication a
           t least 30 days in advance of sale. If applicable, the notice must identify t
@@ -64,12 +64,12 @@ const Facilities = () => {
         </p>
       </div>
 
-      <div className="facilities-container">
+      <div className="facilitiescontainer">
         <CardContainer cards={cardsData} icons={icons} />
       </div>
 
       {showButton && <div>
-        <button className='facilities-button1' onClick={handleClick}> SEE ALL</button>
+        <button className='facilitiesbutton1' onClick={handleClick}> SEE ALL</button>
       </div>}
 
       {showList &&  <ColumnList />}
--- src/components/Facilities/facilitiesList.js
+++ src/components/Facilities/facilitiesList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './facilities.css';
-import { FaBath, FaBed, FaTree, FaUtensils, FaFootballBall, FaCouch, FaTv, FaWineGlassAlt, FaWifi, FaParking, FaHandHoldingHeart, FaUserShield, FaInfoCircle, FaLanguage } from 'react-icons/fa';
+import { FaBath, FaBed, FaTree, FaUtensils,FaMicrowave, FaFootballBall, FaCouch, FaTv, FaWineGlassAlt, FaWifi, FaParking, FaHandHoldingHeart,FaHome, FaUserShield, FaInfoCircle, FaLanguage,FaGlobe } from 'react-icons/fa';
 
 const ColumnList = () => {
   return (
-    <div>
-    <div className="fs-column-list">
+    <div >
+    <div className="fscolumnlist">
       
 
-      <div className="fs-column">
+      <div className="fscolumn">
         
-        <h2><FaBath /> Bathroom</h2>
+        <h2 className='fscolumnh2'><FaBath /> Bathroom</h2>
         <ul>
           <li>Toilet Paper </li>
           <li>Towels</li>
@@ -18,38 +18,38 @@ const ColumnList = () => {
           <li>Hair Dryer</li>
         </ul>
 
-        <h2><FaBed /> Bedroom</h2>
+        <h2 className='fscolumnh2'><FaBed /> Bedroom</h2>
         <ul>
           <li>Linen</li>
           <li>List item 2</li>
         </ul>
-        <h2><FaTree /> Outdoors</h2>
+        <h2 className='fscolumnh2'><FaTree /> Outdoors</h2>
         <ul>
           <li>Outdoor Furniture</li>
           <li>Terrace</li>
         </ul>
-        <h2><FaUtensils /> Kitchen</h2>
+        <h2 className='fscolumnh2'><FaUtensils /> Kitchen</h2>
         <ul>
           <li>Electric Kettle</li>
           <li>Refridgerator</li>
         </ul>
-        <h2><FaFootballBall /> Activities</h2>
+        <h2 className='fscolumnh2'><FaFootballBall /> Activities</h2>
         <ul>
           <li>Live Sports Events</li>
         </ul>
-        <h2><FaCouch />Livining Area</h2>
+        <h2 className='fscolumnh2'><FaCouch />Livining Area</h2>
         <ul>
           <li>Desk</li>
         </ul>
-        <h2><FaTv />Media & Technology</h2>
+        <h2 className='fscolumnh2'><FaTv />Media & Technology</h2>
         <ul>
           <li>Flat Screen TV</li>
           <li>Telephone</li>
           <li>TV</li>
         </ul>
       </div>
-      <div className="fs-column">
-        <h2><FaWineGlassAlt /> Food & Drink</h2>
+      <div className="fscolumn">
+        <h2 className='fscolumnh2'><FaWineGlassAlt /> Food & Drink</h2>
         <ul>
           <li>Coffee house in site</li>
           <li>Wines</li>
@@ -59,15 +59,15 @@ const ColumnList = () => {
           <li>Bar</li>
           <li>Restaurant</li>
         </ul>
-        <h2><FaWifi /> Internet</h2>
+        <h2 className='fscolumnh2'><FaWifi /> Internet</h2>
         <ul>
           <li>Free Wifi it available in all areas</li>
         </ul>
-        <h2><FaParking /> Parking</h2>
+        <h2 className='fscolumnh2'><FaParking /> Parking</h2>
         <ul>
           <li>Free Public parking</li>
         </ul>
-        <h2> Services</h2>
+        <h2 className='fscolumnh2'><FaGlobe/> Services</h2>
         <ul>
           <li>Daily Housekeeping</li>
           <li>Concierge Service</li>
@@ -78,13 +78,13 @@ const ColumnList = () => {
           <li>24 houres fruit desk</li>
           <li>Room Services</li>
         </ul>
-        <h2><FaHandHoldingHeart /> Reception Services</h2>
+        <h2 className='fscolumnh2'><FaHandHoldingHeart /> Reception Services</h2>
         <ul>
           <li>List item 1</li>
         </ul>
       </div>
-      <div className="fs-column">
-        <h2><FaUserShield /> Safety & Security</h2>
+      <div className="fscolumn">
+        <h2 className='fscolumnh2'><FaUserShield /> Safety & Security</h2>
         <ul>
           <li>Fire Exiting</li>
           <li>CCTV outside property</li>
@@ -93,7 +93,7 @@ const ColumnList = () => {
           <li>Key access</li>
           <li>Safety default box</li>
         </ul>
-        <h2><FaInfoCircle /> General</h2>
+        <h2 className='fscolumnh2'><FaHome /> General</h2>
         <ul>
           <li>Designated working area</li>
           <li>Non-</li>
@@ -102,7 +102,7 @@ const ColumnList = () => {
           <li>List item 2</li>
           <li>List item 3</li>
         </ul>
-        <h2><FaLanguage /> Languages spoken</h2>
+        <h2 className='fscolumnh2'><FaGlobe /> Languages spoken</h2>
         <ul>
           <li>English</li>
         </ul>
