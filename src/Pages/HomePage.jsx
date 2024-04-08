039e7ace35caf74bc6d5ef40ccd927ad69e0b7f5
THULIRSHAN98 <thulirshan@iykons.com>
2024-04-08 11:09:46 +0530

    cardesigning going on

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import Header from '../components/common/Header'
 import About from '../components/home/About'
 import HotelRoomCard from '../components/common/HotelRoomCard'
+import img1 from '../assets/images/Loungebar 1.jpg'
 
 export default function HomePage(){
   return (
    <>
    <Header/>
    <About />
-   <HotelRoomCard  /> 
+   <HotelRoomCard
+        imageSource={img1}
+        title="Luxury Room"
+        description="Spacious room with a breathtaking view"
+        guests={2}
+        status="Available"
+        price="$200 per night"
+        buttonText="Book Now"
+      /> 
    </>
   )
 }
