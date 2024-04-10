8cebfde0eabfa2ef11b5c7434afd5d5c1991aefe
THULIRSHAN98 <thulirshan@iykons.com>
2024-04-10 11:01:12 +0530

    added room related task

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import Header from '../components/common/Header'
 import About from '../components/home/About'
-import HotelRoomCard from '../components/common/HotelRoomCard'
-import img1 from '../assets/images/Loungebar 1.jpg'
+import Room from '../components/home/Room'
+
 
 export default function HomePage(){
   return (
    <>
    <Header/>
    <About />
-   <HotelRoomCard
-        imageSource={img1}
-        title="Luxury Room"
-        description="Spacious room with a breathtaking view"
-        guests={2}
-        status="Available"
-        price="$200 per night"
-        buttonText="Book Now"
-      /> 
+    <Room/>
+   
    </>
   )
 }
--- src/components/common/HotelRoomCard.jsx
+++ src/components/common/HotelRoomCard.jsx
@@ -1,21 +1,61 @@
-import React from 'react';
-import '../../css/HotelRoomCard.css'
+import React from "react";
+import "../../css/HotelRoomCard.css";
+import { Row, Col } from "antd";
 
-const HotelRoomCard = ({ imageSource, title, description, guests, status, price, buttonText }) => {
+const HotelRoomCard = ({
+  imageSource,
+  title,
+  description,
+  guests,
+  status,
+  price,
+  buttonText,
+  cardtitle,
+}) => {
   return (
     <div className="cardContainer">
       <img src={imageSource} className="cardImage" alt="Room" />
+      <h1 className="cardTopBar">{cardtitle}</h1>
       <div className="cardDetails">
         <h2 className="cardTitle">{title}</h2>
-        <p className="cardDescription">{description}</p>
-        <div className="cardInfoRow">
-          <p className="cardInfoText">Guests: {guests}</p>
-          <p className="cardInfoText">Status: {status}</p>
-        </div>
-        <div className="cardInfoRow">
-          <p className="cardPriceText">{price}</p>
-          <button className="cardButton">{buttonText}</button>
-        </div>
+        <Row className="hotelRoomCard-MainRow">
+          <Col span={8}>
+            <Row>
+              {" "}
+              <p>GUEST</p>
+            </Row>
+            <Row>
+              <p>01</p>
+            </Row>
+          </Col>
+          <Col span={8}>
+            <Row>
+              {" "}
+              <p>ROOMS</p>
+            </Row>
+            <Row>
+              <p>02</p>
+            </Row>
+          </Col>
+          <Col span={8}>
+            <Row>
+              {" "}
+              <p>AVAIABLE</p>
+            </Row>
+            <Row>
+              <p>01</p>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="cardInfoRow">
+          <Col span={12}>
+            <p className="cardPriceText">{price} per night</p>
+            <p className="cardPriceText2">including taxes</p>
+          </Col>
+          <Col span={12}>
+            <button className="cardButton">{buttonText}</button>
+          </Col>
+        </Row>
       </div>
     </div>
   );
