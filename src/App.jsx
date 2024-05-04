cfd9cf4e317fa4570800e7a302819116ad3cb2c3
radhisew <radhisew@iykons.com>
2024-05-04 18:45:10 +0530

    Basic Room

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./Pages/HomePage";
 import Rooms from "./Pages/Rooms";
 import Footer from "./components/common/Footer"
 import Facilities from "./Pages/Facilities";
+import BasicRoom from "./components/Room/BasicRoom";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/rooms" element={<Rooms />} />
           <Route path="/Facilities" element={<Facilities />} />
+          <Route path="/BasicRoom" element={<BasicRoom/>}/>
         </Routes>
       </Router>
     </div>
