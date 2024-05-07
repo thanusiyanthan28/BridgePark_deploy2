c85b85df90dabf74a5874f6125be95a1f2c607c9
thanusiyanthan28 <thanusiyanthan@iykons.com>
2024-05-07 10:25:03 +0530

    room type link update

--- src/components/home/RoomType.jsx
+++ src/components/home/RoomType.jsx
@@ -1,3 +1,36 @@
+// import React from "react";
+// import HotelRoomTypeCard from "../common/HotelRoomTypeCard";
+// import img1 from "../../assets/images/ExecutiveKingRoom.jpg";
+
+// const RoomType = () => {
+//   return (
+//     <div>
+//       <HotelRoomTypeCard
+//         imageSource={img1}
+//         title="Single Room"
+//         description="All the rooms “Basic” are with shared bathroom facilities"
+//         guests={2}
+//         status="Available"
+//         price="$200"
+//         buttonText="Book Now"
+//         cardtitle="Basic Rooms"
+//       />
+//       <HotelRoomTypeCard
+//         imageSource={img1}
+//         title="Single Room"
+//         description="“En-suite” rooms are with Private bathroom facilties"
+//         guests={2}
+//         status="Available"
+//         price="$200"
+//         buttonText="Book Now"
+//         cardtitle="Ensuite Rooms"
+//       />
+//     </div>
+//   );
+// };
+
+// export default RoomType;
+
 import React from "react";
 import HotelRoomTypeCard from "../common/HotelRoomTypeCard";
 import img1 from "../../assets/images/ExecutiveKingRoom.jpg";
@@ -14,16 +47,18 @@ const RoomType = () => {
         price="$200"
         buttonText="Book Now"
         cardtitle="Basic Rooms"
+        linkTo="/BasicRoom"
       />
       <HotelRoomTypeCard
         imageSource={img1}
         title="Single Room"
-        description="“En-suite” rooms are with Private bathroom facilties"
+        description="“En-suite” rooms are with Private bathroom facilities"
         guests={2}
         status="Available"
         price="$200"
         buttonText="Book Now"
         cardtitle="Ensuite Rooms"
+        linkTo="/EnsuiteRoom"
       />
     </div>
   );
