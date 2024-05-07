9e010fb83e0be2258a34744e6b61f94a664a5e7d
radhisew <radhisew@iykons.com>
2024-05-07 14:21:14 +0530

    HomeRules

--- src/components/common/HomeRules.jsx
+++ src/components/common/HomeRules.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "../../css/HouseRules.css";
-
+import exportImg from "../../assets/icons/export.gif";
+import cancelImg from "../../assets/icons/age-restriction.gif";
+import childrenBed from "../../assets/icons/Children & Beds.gif";
+import ageImg from "../../assets/icons/age-restriction.gif";
+import pets from "../../assets/icons/Pets.gif";
+import groupImg from "../../assets/icons/Group.gif";
+import acceptPay from "../../assets/icons/Accepted payment methods.gif";
 const HouseRules = () => {
   return (
     <div className="house-rules-container">
@@ -12,67 +18,148 @@ const HouseRules = () => {
         Bridge Park Hotel takes special requests - add in the next step!
       </p>
       <div className="house-rules-container-body">
-        <div class="house-rules-section">
-          <div class="house-rules-item">Check-in</div>
-          <ul class="house-rules-info">
-            <li>From 14:00</li>
-            <li>
-              Guests are required to show a photo identification and credit card
-              upon check-in
-            </li>
-          </ul>
-          <hr class="house-rules-divider" />
-        </div>
+        <table class="house-rules-table">
+          <tr>
+            <td>
+              <img
+                src={exportImg}
+                alt="Check-in Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Check-in</td>
+            <td>
+              <ul className="house-rules-table-ul">
+                <li>From 14:00</li>
+                <li>
+                  Guests are required to show a photo identification and credit
+                  card upon check-in
+                </li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src={exportImg}
+                alt="Check-out Icon"
+                class="house-rules-icon-rotate"
+              />
+            </td>
+            <td className="house-rules-info">Check-out</td>
+            <td>From 14:00</td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src={cancelImg}
+                alt="Cancellation Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Cancellation/Prepayment</td>
+            <td>
+              Cancellation and prepayment policies vary according to
+              accommodation type. Please check what conditions may apply to each
+              option when making your selection.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src={cancelImg}
+                alt="Cancellation Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Refundable damage deposit</td>
+            <td>
+              A damage deposit of GBP 100 is required on arrival. That's about
+              LKR 37283. This will be collected by credit card. You should be
+              reimbursed on check-out. Your deposit will be refunded in full via
+              credit card, subject to an inspection of the property.
+            </td>
+          </tr>
 
-        {/* <div className="house-rules-section">
-          <div className="house-rules-item house-rules-icon">➔ Check-out</div>
-          <p className="house-rules-info">Until 11:00</p>
-          <hr className="house-rules-divider" />
-        </div> */}
-        {/* <div className="house-rules-section">
-          <div className="house-rules-item house-rules-icon">
-            ⓘ Cancellation/ prepayment
-          </div>
-          <p className="house-rules-info">
-            Cancellation and prepayment policies vary according to accommodation
-            type. Please check what
-            <a href="#" className="house-rules-link">
-              conditions
-            </a>{" "}
-            may apply to each option when making your selection.
-          </p>
-          <hr className="house-rules-divider" />
-        </div> */}
-        {/* <div className="house-rules-section">
-          <div className="house-rules-item house-rules-icon">
-            ⓘ Refundable damage deposit
-          </div>
-          <p className="house-rules-info">
-            A damage deposit of GBP 100 is required on arrival. That's about LKR
-            37283. This will be collected by credit card. You should be
-            reimbursed on check-out. Your deposit will be refunded in full via
-            credit card, subject to an inspection of the property.
-          </p>
-          <hr className="house-rules-divider" />
-        </div> */}
-        {/* <div className="house-rules-section">
-          <div className="house-rules-item house-rules-icon">
-            👶 Children and beds
-          </div>
-          <p className="house-rules-info">Child policies</p>
-          <p className="house-rules-info">Children of any age are welcome.</p>
-          <p className="house-rules-info">
-            Children 13 years and above will be charged as adults at this
-            property.
-          </p>
-          <p className="house-rules-info">
-            To see correct prices and occupancy information, please add the
-            number of children in your group and their ages to your search.
-          </p>
-          <p className="house-rules-info font-medium">
-            Cot and extra bed policies
-          </p>
-        </div> */}
+          <tr>
+            <td>
+              <img
+                src={childrenBed}
+                alt="Childern & bed Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Children and beds</td>
+            <td>
+              <ul>
+                <li className="house-rule-sub-topic">Child policies</li>
+                <li>Children of any age are welcome.</li>
+                <li>
+                  Children 13 years and above will be charged as adults at this
+                  property.
+                </li>
+                <li>
+                  To see correct prices and occupancy information, please add
+                  the number of children in your group and their ages to your
+                  search.
+                </li>
+                <li className="house-rule-sub-topic">Cot and extra bed policies</li>
+                <li>Cots and extra beds are not available at this property.</li>
+              </ul>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                src={ageImg}
+                alt="Age restriction Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Age restriction</td>
+            <td>The minimum age for check-in is 18</td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                src={pets}
+                alt="Pets Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Pets</td>
+            <td>Pets are not allowed.</td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                src={groupImg}
+                alt="Group Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Groups</td>
+            <td>
+              When booking more than 5 rooms, different policies and additional
+              supplements may apply.
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                src={acceptPay}
+                alt="Accepted payment methods Icon"
+                class="house-rules-icon"
+              />
+            </td>
+            <td className="house-rules-info">Accepted payment methods</td>
+            <td>Card Images</td>
+          </tr>
+        </table>
       </div>
     </div>
   );
