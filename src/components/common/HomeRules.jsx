a2af04967084d7df009be6d1076b770f34daa567
Dussanthan01 <dussanthan99@iykons.com>
2024-05-10 11:41:35 +0530

    new updates

--- src/components/common/HomeRules.jsx
+++ src/components/common/HomeRules.jsx
@@ -8,6 +8,9 @@ import pets from "../../assets/icons/Pets.gif";
 import groupImg from "../../assets/icons/Group.gif";
 import acceptPay from "../../assets/icons/Accepted payment methods.gif";
 import refundabaleImg from "../../assets/icons/Refundable DAmage.gif";
+import amaricanex from "../../assets/Facilities of BPH/amaricanex.jpg"
+import master from "../../assets/Facilities of BPH/master.png"
+import visa from "../../assets/Facilities of BPH/visa.jpeg"
 const HouseRules = () => {
   return (
     <div className="house-rules-container">
@@ -158,7 +161,27 @@ const HouseRules = () => {
               />
             </td>
             <td className="house-rules-info">Accepted payment methods</td>
-            <td>Card Images</td>
+            <td> <img
+                src={amaricanex}
+                alt="Accepted payment methods Icon"
+                class="house-rules-icon"/>
+                <img
+                src={master}
+                alt="Accepted payment methods Icon"
+                class="house-rules-icon"/>
+                <img
+                src={visa}
+                alt="Accepted payment methods Icon"
+                class="house-rules-icon"/>
+                <img
+                src={visa}
+                alt="Accepted payment methods Icon"
+                class="house-rules-icon"/>
+                <img
+                src={visa}
+                alt="Accepted payment methods Icon"
+                class="house-rules-icon"/>
+            </td>
           </tr>
         </table>
       </div>
