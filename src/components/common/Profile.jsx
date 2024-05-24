2c54ad42d337869f19dc7ee75b681a76f4a4059a
radhisew <radhisew@iykons.com>
2024-05-24 10:59:05 +0530

    Finalprofile

--- src/components/common/Profile.jsx
+++ src/components/common/Profile.jsx
@@ -1,119 +1,82 @@
-import React, { useState } from 'react';
-import { useCookies } from 'react-cookie';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Avatar, Menu, Dropdown, Button, Space } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import { AntDesignOutlined, ProfileOutlined, BellOutlined, SecurityScanOutlined, LoginOutlined } from '@ant-design/icons';
-import { Avatar, Badge, Space, Dropdown, Menu, Button } from 'antd';
-import '../../css/Profile.css';
+import "../../css/Profile.css";
 
-const [cookies, setCookie, removeCookie] = useCookies(['user']);
-const [isLoggedIn, setIsLoggedIn] = useState(!!cookies.user);
-
-const handleLogin = () => {
-  setCookie('user', 'John Doe', { path: '/' });
-  setIsLoggedIn(true);
-};
-
-const handleLogout = () => {
-  removeCookie('user');
-  setIsLoggedIn(false);
-};
-
-const BeforeProfile = () => {
-  const [popoverVisible, setPopoverVisible] = useState(false);
+const Profile = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showButtons, setShowButtons] = useState(false);
   const navigate = useNavigate();
 
-  const handleAvatarClick = () => {
-    setPopoverVisible(!popoverVisible);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    setShowButtons(false);
   };
 
-  const content = (
-    <Space direction="vertical">
-      <Button onClick={handleLogin} className="BeforeProfileBtn">
-        Sign In
-      </Button>
-      <Button onClick={() => {}} className="BeforeProfileBtn">
-        Sign Up
-      </Button>
-    </Space>
-  );
-
-  return (
-    <div className="profile-avatar-container">
-      <Space>
-        <Dropdown overlay={content} visible={popoverVisible} placement="bottomRight" trigger={['click']}>
-          <span onClick={handleAvatarClick}>
-            <Avatar
-              className="profile-avatar"
-              icon={<AntDesignOutlined />}
-            />
-          </span>
-        </Dropdown>
-      </Space>
-    </div>
-  );
-};
-
-const CustomProfile = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  const handleAvatarClick = () => {
-    setMenuVisible(!menuVisible);
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    navigate('/');
   };
-
-  const handleMenuClick = (e) => {
-    console.log('Menu item clicked:', e);
-    setMenuVisible(false);
-  };
-
-  const items = [
-    {
-      label: 'My Booking',
-      key: '1',
-      icon: <ProfileOutlined />,
-    },
-    {
-      label: 'Notifications',
-      key: '2',
-      icon: <BellOutlined />,
-    },
-    {
-      label: 'Help',
-      key: '3',
-      icon: <SecurityScanOutlined />,
-    },
-    {
-      label: 'Logout',
-      key: '4',
-      icon: <LoginOutlined />,
-    },
-  ];
 
   const menu = (
-    <Menu onClick={handleMenuClick} className="profile-custom-dropdown">
-      {items.map((item) => (
-        <Menu.Item key={item.key} icon={item.icon} danger={item.danger} disabled={item.disabled}>
-          {item.label}
-        </Menu.Item>
-      ))}
+    <Menu>
+      <Menu.Item key="0"className="custom-menu-item">
+      <ProfileOutlined className="Profile-icon"/>
+        <Link to="/my-booking">My Booking</Link>
+      </Menu.Item>
+      <Menu.Item key="1" className="custom-menu-item">
+      <BellOutlined className="Profile-icon"/>
+        <Link to="/notifications">Notifications</Link>
+      </Menu.Item>
+      <Menu.Item key="2" className="custom-menu-item">
+      <SecurityScanOutlined className="Profile-icon"/>
+        <Link to="/help">Help</Link>
+      </Menu.Item>
+      <Menu.Item key="3" onClick={handleLogout} className="custom-menu-item">
+      <LoginOutlined className="Profile-icon"/>
+        Logout
+      </Menu.Item>
     </Menu>
   );
 
   return (
-    <div className="profile-avatar-container">
-      <Space>
-        <Dropdown overlay={menu} visible={menuVisible} placement="bottomRight" trigger={['click']}>
-          <span onClick={handleAvatarClick}>
-            <Badge size="small" count={1}>
-              <Avatar
-                className="profile-avatar"
-                icon={<AntDesignOutlined />}
-              />
-            </Badge>
-          </span>
-        </Dropdown>
-      </Space>
+    <div className="profile-wrapper">
+      <div className="profile-container">
+        {!isLoggedIn ? (
+          <div>
+            <Avatar
+              size="large"
+              icon={<UserOutlined />}
+              onClick={() => setShowButtons(!showButtons)}
+              className="profile-image"
+            />
+            {showButtons && (
+              <div className="buttons-container">
+                <Space direction="vertical">
+                  <Link to="/SignIn">
+                    <Button onClick={handleLogin} className="custom-button">Sign In</Button>
+                  </Link>
+                  <Link to="/SignUp">
+                    <Button className="custom-button">Sign Up</Button>
+                  </Link>
+                </Space>
+              </div>
+            )}
+          </div>
+        ) : (
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Avatar
+              size="large"
+              icon={<UserOutlined />}
+              className="profile-image"
+            />
+          </Dropdown>
+        )}
+      </div>
     </div>
   );
 };
 
-export default CustomProfile;
+export default Profile;
