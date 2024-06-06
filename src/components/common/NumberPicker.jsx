ca6fd41132557dfedfe204b2e90d8e6d8e4ae878
thanusiyanthan28 <thanusiyanthan@iykons.com>
2024-06-06 14:09:47 +0530

    update

--- src/components/common/NumberPicker.jsx
+++ src/components/common/NumberPicker.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Input, Space } from "antd";
+import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
 
 function NumberPicker({ placeholder, value, onChange, min = 0 }) {
   const increment = () => onChange(value + 1);
@@ -9,14 +10,26 @@ function NumberPicker({ placeholder, value, onChange, min = 0 }) {
 
   return (
     <Space>
-      <Button onClick={decrement}>-</Button>
+      <Button 
+        shape="circle" 
+        onClick={decrement} 
+        style={{ border: "none", backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+      >
+        <MinusOutlined style={{ color: '#669399', fontWeight: 'bold',fontSize:'15px' }} />
+      </Button>
       <Input 
         value={value} 
         readOnly 
-        style={{ width: '60px', textAlign: 'center' }} 
+        style={{ width: '60px', textAlign: 'center', border: "none", fontSize: '25px', fontWeight: 'bold' }} 
         placeholder={placeholder} 
       />
-      <Button onClick={increment}>+</Button>
+      <Button 
+        shape="circle" 
+        onClick={increment} 
+        style={{ border: "none", backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+      >
+        <PlusOutlined style={{ color: '#669399', fontWeight: 'bold',fontSize:'15px' }} />
+      </Button>
     </Space>
   );
 }
