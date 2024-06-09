74e7a8b4a33663d4e68e43e7b1503230224bbd3e
radhisew <radhisew@iykons.com>
2024-06-09 13:21:26 +0530

    Update

--- src/components/home/RoomType.jsx
+++ src/components/home/RoomType.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import HotelRoomTypeCard from "../common/HotelRoomTypeCard";
-// import img1 from "../../assets/images/ExecutiveKingRoom.jpg";
-
-// const RoomType = () => {
-//   return (
-//     <div>
-//       <HotelRoomTypeCard
-//         imageSource={img1}
-//         title="Single Room"
-//         description="All the rooms “Basic” are with shared bathroom facilities"
-//         guests={2}
-//         status="Available"
-//         price="$200"
-//         buttonText="Book Now"
-//         cardtitle="Basic Rooms"
-//       />
-//       <HotelRoomTypeCard
-//         imageSource={img1}
-//         title="Single Room"
-//         description="“En-suite” rooms are with Private bathroom facilties"
-//         guests={2}
-//         status="Available"
-//         price="$200"
-//         buttonText="Book Now"
-//         cardtitle="Ensuite Rooms"
-//       />
-//     </div>
-//   );
-// };
-
-// export default RoomType;
-
 import React, { useState, useEffect } from "react";
 import HotelRoomTypeCard from "../common/HotelRoomTypeCard";
 import img1 from "../../assets/images/ExecutiveKingRoom.jpg";
