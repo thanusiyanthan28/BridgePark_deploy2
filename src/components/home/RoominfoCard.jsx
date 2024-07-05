504c893c049a5d1832931c2a35997d40ee7634fb
Dussanthan01 <dussanthan99@iykons.com>
2024-07-05 15:15:31 +0530

    new

--- src/components/home/RoominfoCard.jsx
+++ src/components/home/RoominfoCard.jsx
@@ -8,16 +8,16 @@ import img5 from "../../assets/images/ExecutiveKingRoom.jpg"
 import img6 from "../../assets/images/ExecutiveKingRoom 2.jpg"
 import img7 from "../../assets/images/Tripleroomwithsharedbathroom.jpg"
 import img8 from "../../assets/images/QuadEnsuitedoubleandtwosinglebeds.jpg"
-import '../../css/RoomInformation/RoominfoCard.css'
+import '../../css/RoomInformation/RoominfoCard.css';
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 430);
 
-  
   const images = [
     [img1, img2, img3],  // Images for the first card
-    [img4, img5, img6,],  // Images for the second card
-    [img7, img8, img1,]   // Images for the third card
+    [img4, img5, img6],  // Images for the second card
+    [img7, img8, img1]   // Images for the third card
   ];
 
   // Automatically change slide every 3 seconds
@@ -28,24 +28,37 @@ const Carousel = () => {
     return () => clearInterval(interval);
   }, [images]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 430);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="room-info-cards">
       <Row gutter={[16, 16]}>
-        <Col span={12}>
+        <Col span={isSmallScreen ? 24 : 12}>
           <Card className="info-card">
             <img src={images[0][currentIndex]} alt={`Card 1 - Image ${currentIndex + 1}`} />
           </Card>
         </Col>
-        <Col span={6}>
-          <Card className="info-card">
-            <img src={images[1][currentIndex]} alt={`Card 2 - Image ${currentIndex + 1}`} />
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card className="info-card">
-            <img src={images[2][currentIndex]} alt={`Card 3 - Image ${currentIndex + 1}`} />
-          </Card>
-        </Col>
+        {!isSmallScreen && (
+          <>
+            <Col span={6} className="hide-on-small-screen">
+              <Card className="info-card">
+                <img src={images[1][currentIndex]} alt={`Card 2 - Image ${currentIndex + 1}`} />
+              </Card>
+            </Col>
+            <Col span={6} className="hide-on-small-screen">
+              <Card className="info-card">
+                <img src={images[2][currentIndex]} alt={`Card 3 - Image ${currentIndex + 1}`} />
+              </Card>
+            </Col>
+          </>
+        )}
       </Row>
     </div>
   );
