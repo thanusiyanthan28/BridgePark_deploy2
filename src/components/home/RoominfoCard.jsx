c9b281ae0d2fbea72e50ec77788abf4f78f9a891
Dussanthan01 <dussanthan99@iykons.com>
2024-07-09 09:19:23 +0530

    gh

--- src/components/home/RoominfoCard.jsx
+++ src/components/home/RoominfoCard.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col, Card } from 'antd';
-import img1 from "../../assets/images/SingleRoomwithSharedBathroom.jpg";
-import img2 from "../../assets/images/DoubleEnsuite.jpg";
-import img3 from "../../assets/images/Tripleroomwithsharedbathroom.jpg";
-import img4 from "../../assets/images/TripleEnsuite3singlebeds.jpg";
-import img5 from "../../assets/images/ExecutiveKingRoom.jpg"
-import img6 from "../../assets/images/ExecutiveKingRoom 2.jpg"
-import img7 from "../../assets/images/Tripleroomwithsharedbathroom.jpg"
-import img8 from "../../assets/images/QuadEnsuitedoubleandtwosinglebeds.jpg"
+import img1 from "../../assets/RoomTypes/SingleRoom.webp";
+import img2 from "../../assets/RoomTypes/DoubleEnsuiteRoom2.webp";
+import img3 from "../../assets/RoomTypes/TwlightRoom5.webp";
+import img4 from "../../assets/RoomTypes/TwinRoom4.webp";
+import img5 from "../../assets/RoomTypes/TwinEnsuite2.webp";
+import img6 from "../../assets/RoomTypes/TripleRoomEnsuite3.webp";
+import img7 from "../../assets/RoomTypes/DoubleRoom2.webp";
+import img8 from "../../assets/RoomTypes/TripleRoom1.webp";
 import '../../css/RoomInformation/RoominfoCard.css';
 
 const Carousel = () => {
@@ -42,19 +42,25 @@ const Carousel = () => {
       <Row gutter={[16, 16]}>
         <Col span={isSmallScreen ? 24 : 12}>
           <Card className="info-card">
-            <img src={images[0][currentIndex]} alt={`Card 1 - Image ${currentIndex + 1}`} />
+            <div className="image-container">
+              <img src={images[0][currentIndex]} alt={`Card 1 - Image ${currentIndex + 1}`} />
+            </div>
           </Card>
         </Col>
         {!isSmallScreen && (
           <>
             <Col span={6} className="hide-on-small-screen">
               <Card className="info-card">
-                <img src={images[1][currentIndex]} alt={`Card 2 - Image ${currentIndex + 1}`} />
+                <div className="image-container">
+                  <img src={images[1][currentIndex]} alt={`Card 2 - Image ${currentIndex + 1}`} />
+                </div>
               </Card>
             </Col>
             <Col span={6} className="hide-on-small-screen">
               <Card className="info-card">
-                <img src={images[2][currentIndex]} alt={`Card 3 - Image ${currentIndex + 1}`} />
+                <div className="image-container">
+                  <img src={images[2][currentIndex]} alt={`Card 3 - Image ${currentIndex + 1}`} />
+                </div>
               </Card>
             </Col>
           </>
