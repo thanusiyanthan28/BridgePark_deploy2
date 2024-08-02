218f5de4b56c45aa72526d64f24f4e4f21dca074
thanusiyanthan28 <165994199+thanusiyanthan28@users.noreply.github.com>
2024-08-02 17:01:23 +0530

    Update MainMeeting.jsx

--- src/Pages/MeetingAndEvents/MainMeeting.jsx
+++ src/Pages/MeetingAndEvents/MainMeeting.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ImageSlider1 from './ImageSlider1'
 import PanoImage from './PanoImage'
-import NewMeeting from './NewMeeting'
+import NewMeeting from './Newmeeting'
 import ImageSlider2 from './ImageSlider2'
 
 const MainMeeting = () => {
